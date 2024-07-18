e82e21e01911bed2f2a25eee9248a6b96c590aa7
Sakshi <gangwani.sakshi15@gmail.com>
2024-07-18 16:18:18 +0530

    created the UI and logic

--- myapp/App.js
+++ myapp/App.js
@@ -1,28 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, ScrollView, Pressable, Modal, Image } from 'react-native';
+import GoalItem from './components/GoalItem';
 
 export default function App() {
-  const [goalhandler, setGoalHandler] = useState("");
+  const [goalhandler, setGoalHandler] = useState('');
   const [courseGoals, setCourseGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
-  function handleGoal(enteredText){
-    setGoalHandler(enteredText);
+  function StartAddGoalHandler() {
+    setModalIsVisible(true);
   }
 
-  function addGoalHandler(){
-    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, goalhandler]);
-    console.log(courseGoals);
+  function EndAddGoalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function handleGoal(enteredText){
+    setGoalHandler(enteredText); 
+  }
+
+  function addGoalHandler() {
+    const newGoal = {
+      id: courseGoals.length === 0 ? 1 : courseGoals[courseGoals.length - 1].id + 1,
+      goalName: goalhandler,
+    };
+    setCourseGoals((currentCourseGoals) => [...currentCourseGoals, newGoal]);
+    setGoalHandler('');
+    EndAddGoalHandler();
+  }
+
+  function deleteGoal(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.id !== id);
+    });
   }
 
   return (
-    <View style={styles.appContainer}>
+  <View style={styles.appContainer}>
+    <View style={styles.addNewGoal}>
+      <Button title='Add New Goal' color="white" onPress={StartAddGoalHandler}/>
+    </View>
+    {modalIsVisible && <Modal animationType='slide'>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={handleGoal}/>
-        <Button title='Add Goal' onPress={addGoalHandler}/>
+        <Image style={styles.image} source={require('.//assets//images//goal.png')} />
+        <TextInput style={styles.textInput} placeholder='Your course goal!' onChangeText={handleGoal} value={goalhandler}/>
+        <View style={styles.buttonContainer}>
+          <View style={styles.buttonAdd}>
+            <Button color='white' title='Add Goal' onPress={addGoalHandler}/>
+          </View>
+          <View style={styles.buttonCancel}>
+            <Button color='white' title='Cancel' onPress={EndAddGoalHandler}/>
+          </View>
+        </View>
       </View>
+    </Modal>}
       <View style={styles.goalsContainer}>
-        {courseGoals.map((goal) => <View  style={styles.goalText}><Text style={{color: 'white'}}>{goal}</Text></View>)}
+      <ScrollView>
+          {courseGoals.map((goal) => (
+            <Pressable key={goal.id} onPress={() => deleteGoal(goal.id)}>
+              <View style={styles.goalText}>
+                <Text style={{ color: '#5e0acc' }}>{goal.goalName}</Text>
+              </View>
+            </Pressable>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -36,27 +78,50 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     alignItems: 'center',
-    marginBottom: 24,
-    borderBottomWidth: 1,
-    borderBottomColor: '#cccccc'
+    padding: 20,
+    backgroundColor: '#311b6b'
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+  },
+  buttonAdd: {
+    width: '30%',
+    marginHorizontal: 8,
+    backgroundColor: '#5e0acc',
+  },
+  buttonCancel: {
+    width: '30%',
+    marginHorizontal: 8,
+    backgroundColor: '#f31282',
   },
   textInput: {
-    borderWidth: 1,
-    width: '70%',
-    backgroundColor: '#e0ffff',
+    borderWidth: 2,
+    borderColor: '#e6e6fa',
+    width: '100%',
+    backgroundColor: '#e6e6fa',
+    borderRadius: 8,
     padding: 15,
-    marginRight: 8,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    margin: 20
   },
   goalsContainer: {
-    flex: 7
+    flex: 5
   },
   goalText: {
-    backgroundColor: 'blue',
+    backgroundColor: '#e6e6fa',
     padding: 16,
     borderRadius: 10,
-    marginTop: 10,
+    marginTop: 14,
+  },
+  addNewGoal: {
+    marginTop: 30,
+    backgroundColor: '#5e0acc',
+    borderRadius: 5,
   }
 });
